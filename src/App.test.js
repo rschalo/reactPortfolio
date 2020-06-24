45ed2b6bff1f4192db088d89e9d65cec73a02f40
rschalo <reed0schalo@gmail.com>
2020-06-24 11:25:42 -0700

    fix: target=_blank security issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,12 @@
-test('fake test', () => (expect(true).toBeTruthy()));
+import React from 'react';
+import Enzyme, { shallow, mount } from 'enzyme';
+import Nav from './';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders', () => {
+  const nav = shallow(<Nav />);
+
+  expect(nav.exists()).toBe(true);
+});
--- src/ContactMe.js
+++ src/ContactMe.js
@@ -15,7 +15,7 @@ const ContactMe = () => (
           justifyContent: 'center',
         }}
       >
-        <a href='mailto:reed0schalo@gmail.com?subject=Resume%20Interest' target='_blank'>
+        <a href='mailto:reed0schalo@gmail.com?subject=Resume%20Interest' target='_blank' rel="noopener noreferrer">
           <FontAwesomeIcon
             className='social-icons'
             icon={faEnvelopeOpenText}
@@ -37,7 +37,7 @@ const ContactMe = () => (
         }}
       >
         <li style={{ margin: '4px' }}>
-          <a href='https://github.com/rschalo' target='_blank'>
+          <a href='https://github.com/rschalo' target='_blank' rel="noopener noreferrer">
             <FontAwesomeIcon
               className='social-icons'
               icon={faGithubAlt}
@@ -47,7 +47,7 @@ const ContactMe = () => (
           </a>
         </li>
         <li>
-          <a href='https://www.linkedin.com/in/reed-schalo/' target='_blank'>
+          <a href='https://www.linkedin.com/in/reed-schalo/' target='_blank' rel="noopener noreferrer">
             <FontAwesomeIcon
               className='social-icons'
               icon={faLinkedinIn}
